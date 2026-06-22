96b8c08393547eb75d25a363208a3a7b0d7653e2
tommaso.cavini <tommaso.cavini@sitti.it>
2026-06-22 23:32:15 +0200

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Epicode | Computer Engineering & Artificial Intelligence</h1>
+      <h1>Frontend Project | Vite + React + Typescript</h1>
+      <h1>App Landing Page</h1>
+    </>
+  );
+}
+
+export default App;
