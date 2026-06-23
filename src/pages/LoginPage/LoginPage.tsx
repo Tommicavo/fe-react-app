fd3efe6026f90dbbb8afb1f5912ac8a535e3b1dc
tommasocavini <tommaso.cavini@sitti.it>
2026-06-23 11:27:11 +0200

    added env file for secrets and defined vite.env.s.ts

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,7 @@
 function LoginPage() {
+  const baseUrl = import.meta.env.VITE_API_BASE_URL;
+  console.log(`Test env variable import: baseUrl: ${baseUrl}`);
+
   return (
     <div className="container">
       <h1>Login Page</h1>
