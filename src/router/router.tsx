08cce5a66d7d3a6d58fcab2743273ecd1a14eb19
Tommicavo <tommicava@icloud.com>
2026-06-24 22:45:18 +0200

    defined login page as frist page and all other pages children of App

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import HomePage from "@/pages/HomePage/HomePage";
+import App from "@/components/App/App";
 import LoginPage from "@/pages/LoginPage/LoginPage";
+import HomePage from "@/pages/HomePage/HomePage";
 import AdminPage from "@/pages/AdminPage/AdminPage";
 import SettingPage from "@/pages/SettingPage/SettingPage";
 import DetailPage from "@/pages/DetailPage/DetailPage";
@@ -9,28 +10,19 @@ import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
     path: "/login",
     element: <LoginPage />,
   },
   {
-    path: "/admin",
-    element: <AdminPage />,
-  },
-  {
-    path: "/setting",
-    element: <SettingPage />,
-  },
-  {
-    path: "/detail/:id",
-    element: <DetailPage />,
-  },
-  {
-    path: "*",
-    element: <NotFoundPage />,
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "admin", element: <AdminPage /> },
+      { path: "setting", element: <SettingPage /> },
+      { path: "detail/:id", element: <DetailPage /> },
+      { path: "*", element: <NotFoundPage /> },
+    ],
   },
 ]);
 
