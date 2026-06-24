dd2e8635d10bc60960422eccc9104a463c93702a
Tommicavo <tommicava@icloud.com>
2026-06-24 22:47:34 +0200

    auth provider component wraps everything

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import "./styles/global.scss";
 import router from "./router/router";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <AuthProvider>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </AuthProvider>,
 );
