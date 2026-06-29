e59245c20303f14086422c2f220094dcf93912f0
tommaso.cavini <tommaso.cavini@sitti.it>
2026-06-29 23:35:43 +0200

    store provider wraps everything

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./styles/global.scss";
 import router from "./router/router";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider>
-    <AuthProvider>
+  <Provider store={store}>
+    <ThemeProvider>
       <RouterProvider router={router} />
-    </AuthProvider>
-  </ThemeProvider>
+    </ThemeProvider>
+  </Provider>,
 );
